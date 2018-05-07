0f29e4a8f356406b20c8dfdda546194457a8bf89
avishnev <123gibson321@gmail.com>
2018-05-07 19:29:20 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -24,10 +24,10 @@ void	init_host(t_src *src)
 	if (TTF_Init() < 0)
 		error_manadge((char *)SDL_GetError());
 	src->wind = SDL_CreateWindow("RT_v1", 
-										SDL_WINDOWPOS_UNDEFINED, 
-										SDL_WINDOWPOS_UNDEFINED,
-										WIN_WD, WIN_HG, 
-										SDL_WINDOW_ALLOW_HIGHDPI);
+					SDL_WINDOWPOS_UNDEFINED, 
+					SDL_WINDOWPOS_UNDEFINED,
+					WIN_WD, WIN_HG, 
+					SDL_WINDOW_ALLOW_HIGHDPI);
 	src->surf = SDL_GetWindowSurface(src->wind);
 	src->img_pxl = src->surf->pixels;
 }
