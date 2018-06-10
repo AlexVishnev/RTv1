13255ca96635491b2a004a8f210535e6286c3287
AlexVishnev <123gibson321@gmail.com>
2018-06-10 21:04:06 +0300

    blah blahg blah

--- src/rt_init.c
+++ src/rt_init.c
@@ -56,8 +56,6 @@ void		init_host(t_src *src)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 		error_manadge((char *)SDL_GetError(), 0, NULL);
-	if (TTF_Init() < 0)
-		error_manadge((char *)SDL_GetError(), 0, NULL);
 	src->wind = SDL_CreateWindow("RT_v1",
 					SDL_WINDOWPOS_UNDEFINED,
 					SDL_WINDOWPOS_UNDEFINED,
