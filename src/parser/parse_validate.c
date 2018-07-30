0457ff3393e36deb3a301b92cb8e6504b3ab4c78
Anton Pisotskyi <anton.pisotskyi@globallogic.com>
2018-07-30 16:49:10 +0300

    additional degug inserted into parser

--- src/parser/parse_validate.c
+++ src/parser/parse_validate.c
@@ -4,40 +4,52 @@
 
 # define CLAMP(x) fmin(fmax(x, min), max)
 
-static void please_validate_and_save_enum_of_int(void *src, void *dst, t_field_info *info)
+static void please_validate_and_save_enum_of_int(void *src,
+	void *dst, t_field_info *info)
 {
-	int arr[info->array_size], i, *tmp, min, max;
+	int arr[info->array_size];
+	int i
+	int *tmp;
+	int min;
+	int max;
 
 	max = info->max_abs;
 	min = (info->can_be_signed) ? (-max) : (0);
 	tmp = (int*)src;
 	i = -1;
 	while (++i < info->array_size)
+	{
 		arr[i] = CLAMP(tmp[i]);
+		printf ("original value: %d, CLAMP value: %d", tmp[i], CLAMP(tmp[i]));
+	}
 	memcpy(dst, src, sizeof(arr));
 }
 
-static void please_validate_and_save_enum_of_float(void *src, void *dst, t_field_info *info)
+static void please_validate_and_save_enum_of_float(void *src,
+	void *dst, t_field_info *info)
 {
-	float arr[info->array_size], *tmp;
-	int i, min, max;
+	float arr[info->array_size];
+	float *tmp;
+	int i;
+	int min;
+	int max;
 
 	max = info->max_abs;
 	min = (info->can_be_signed) ? (-max) : (0);
 	tmp = (float*)src;
 	i = -1;
 	while (++i < info->array_size)
+	{
 		arr[i] = CLAMP(tmp[i]);
+		printf ("original value: %f, CLAMP value: %f", tmp[i], CLAMP(tmp[i]));
+	}
 	memcpy(dst, src, sizeof(arr));
 }
 
 void please_validate_and_save(void *src, void *dst, t_field_info *info)
 {
-	if (info->is_array)
-	{
-		if (info->is_int)
-			please_validate_and_save_enum_of_int(src, dst, info);
-		else
-			please_validate_and_save_enum_of_float(src, dst, info);
-	}		
+	if (info->is_int)
+		please_validate_and_save_enum_of_int(src, dst, info);
+	else
+		please_validate_and_save_enum_of_float(src, dst, info);
 }
