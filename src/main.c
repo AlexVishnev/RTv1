7bfbe6661da38702b3c955ec021b64bca104a482
AlexVishnev <123gibson321@gmail.com>
2018-08-01 16:38:08 +0300

    norme main

--- src/main.c
+++ src/main.c
@@ -28,15 +28,14 @@ void		time_delay(void)
 static void	fps_color(t_src *src, unsigned int fps, TTF_Font *ttf,
 	char *fps_str)
 {
-	(void)ttf;
 	if (fps >= 40)
-		src->fps = TTF_RenderText_Solid(src->ttf, fps_str,
+		src->fps = TTF_RenderText_Solid(ttf, fps_str,
 			(SDL_Color){0, 255, 0, 255});
 	else if (fps >= 20 && fps < 40)
-		src->fps = TTF_RenderText_Solid(src->ttf, fps_str,
+		src->fps = TTF_RenderText_Solid(ttf, fps_str,
 			(SDL_Color){255, 156, 127, 255});
 	else
-		src->fps = TTF_RenderText_Solid(src->ttf, fps_str,
+		src->fps = TTF_RenderText_Solid(ttf, fps_str,
 			(SDL_Color){255, 0, 0, 255});
 }
 
@@ -75,7 +74,7 @@ void		exit_work(t_src *src)
 	free(src->objects);
 	free(src->params.object);
 	free(src->buffer);
-	// system("leaks -q RT");
+	system("leaks -q RT");
 }
 
 int			main(int ac, char **av)
@@ -87,53 +86,10 @@ int			main(int ac, char **av)
 	parser_main(av[1], &src);
 	init_host(&src);
 	init_parametrs(&src);
-
-
-
-	printf("BEFORE WHILE___\n\n\n\nCAMERA:\n");
-	printf("BEFORE WHILE___positon:\n");
-	printf("BEFORE WHILE___[%f, %f, %f]\n", src.params.o.x,src.params.o.y , src.params.o.z );
-	printf("BEFORE WHILE___rotation:\n");
-	printf("BEFORE WHILE___[%f, %f, %f]\n\n", src.params.d.x,src.params.d.y , src.params.d.z );
-
-	printf("BEFORE WHILE___num_lights : %d\n", src.lights_cnt);
-	printf("BEFORE WHILE___num_objects: %d\n\n", src.objects_cnt);
-
-	int i;
-	printf("BEFORE WHILE___LIGHTS:\n");
-	for (i = 0; i < src.lights_cnt; i++)
-	{
-		printf("BEFORE WHILE___type      = %d\n", src.params.light[i].type);
-		printf("BEFORE WHILE___intensive = %f\n", src.params.light[i].intensive);
-		printf("BEFORE WHILE___position  = [%f, %f, %f]\n", src.params.light[i].position.x, 
-					src.params.light[i].position.y , src.params.light[i].position.z );
-		printf("BEFORE WHILE___direction = [%f, %f, %f]\n\n", src.params.light[i].direction.x, 
-					src.params.light[i].direction.y , src.params.light[i].direction.z );
-	}
-	printf("BEFORE WHILE___OBJECTS:\n");
-	for (i = 0; i < src.objects_cnt; i++)
-	{
-		printf("BEFORE WHILE___type      = %d\n", src.params.object[i].type);
-		printf("BEFORE WHILE___size      = %f\n", src.params.object[i].radius);
-		printf("BEFORE WHILE___specular  = %f\n", src.params.object[i].specular);
-		printf("BEFORE WHILE___angle     = %f\n", src.params.object[i].angle);
-		printf("BEFORE WHILE___reflection= %f\n", src.params.object[i].reflection);
-		printf("BEFORE WHILE___position  = [%f, %f, %f]\n", src.params.object[i].mid.x, 
-					src.params.object[i].mid.y , src.params.object[i].mid.z );
-		printf("BEFORE WHILE___rotation  = [%f, %f, %f]\n", src.params.object[i].direction.x, 
-					src.params.object[i].direction.y , src.params.object[i].direction.z );
-		printf("BEFORE WHILE___color     = [%f, %f, %f]\n\n", src.params.object[i].color.x, 
-					src.params.object[i].color.y , src.params.object[i].color.z );
-	}
-	printf("t_min = [%f] t_max = [%f]\n", src.params.t_min, src.params.t_min );
-	printf("src.params.object = %d\nsrc.params.light = %d\n", src.params.objects, src.params.lights);
-	printf("width = %d\n heigth = %d\n",src.params.width, src.params.height );
-
-
 	create_videohost(&src);
 	while (DICK)
 	{
-		if (!expose_hook(&src)) 
+		if (!expose_hook(&src))
 			break ;
 		opencl_kernel_run(&src);
 		init_fps(&src);
